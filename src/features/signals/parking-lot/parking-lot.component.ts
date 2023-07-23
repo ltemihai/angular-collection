5b4e64302a1758da98fe25b253f03c4128ff2d55
mihai.oltean <mihai.oltean@hibob.io>
2023-07-23 15:27:08 +0300

    Added routes

--- src/features/signals/parking-lot/parking-lot.component.ts
+++ src/features/signals/parking-lot/parking-lot.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, computed, Signal, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, effect, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CarType} from "./enums/cartype.enum";
 import {ParkingLot} from "./interfaces/parking-lot.interface";
@@ -15,32 +15,19 @@ import {CarLotComponent} from "./car-lot/car-lot.component";
 })
 export class ParkingLotComponent {
 
-  public cars: Signal<Set<Car>> = signal(new Set([
-    { id: 1, name: 'Tesla Model S', color: 'White', type: CarType.electric },
-    { id: 2, name: 'BMW i3', color: 'Blue', type: CarType.electric },
-    { id: 3, name: 'Toyota Prius', color: 'Silver', type: CarType.electric },
-    { id: 4, name: 'Nissan Leaf', color: 'Green', type: CarType.electric },
-    { id: 5, name: 'Honda Civic', color: 'Red', type: CarType.gas },
-    { id: 6, name: 'Ford Mustang', color: 'Yellow', type: CarType.gas },
-    { id: 7, name: 'Chevrolet Bolt', color: 'Black', type: CarType.electric },
-    { id: 8, name: 'Audi e-tron', color: 'Gray', type: CarType.electric },
-    { id: 9, name: 'Mercedes-Benz C-Class', color: 'White', type: CarType.gas },
-    { id: 10, name: 'Kia Soul', color: 'Orange', type: CarType.gas },
-  ]));
+  public cars = signal(new Set<Car>());
 
-  public parkingLot = signal(new Map<number, ParkingLot>([
-    [1, { type: CarType.electric, car: null }],
-    [2, { type: CarType.electric, car: null }],
-    [3, { type: CarType.gas, car: null }],
-    [4, { type: CarType.gas, car: null }],
-    [5, { type: CarType.gas, car: null }],
-    [6, { type: CarType.gas, car: null }],
-  ]));
+  public parkingLot = signal(new Map<number, ParkingLot>());
 
   public isParkingFree = computed(() => [...this.parkingLot().values()].find(val => val.car === null) === undefined);
   public remainingParkingLots = computed(() => [...this.parkingLot().values()].filter(val => val.car === null).length);
 
   constructor() {
+    this.initializeData();
+
+    effect(() => {
+      console.log(this.parkingLot());
+    })
   }
 
   addCarToParkingLot(car: Car) {
@@ -53,18 +40,40 @@ export class ParkingLotComponent {
     })
   }
 
-
-
   removeCarFromParkingLot(lot: number) {
     this.parkingLot.mutate((parkingLot) => {
-      const car = parkingLot.get(lot)?.car;
+      const car = parkingLot.get(lot)?.car!;
       parkingLot.set(lot, { type: parkingLot.get(lot)?.type!, car: null });
-      this.cars().add(car!);
+      this.cars().add(car);
     })
   }
 
   availableParkingLotsByType(type: CarType | undefined) {
     return [...this.parkingLot().values()].filter(val => val.type === type && val.car === null).length
+  }
+
+  private initializeData() {
+    this.cars.set(new Set([
+      {id: 1, name: 'Tesla Model S', color: 'White', type: CarType.electric},
+      {id: 2, name: 'BMW i3', color: 'Blue', type: CarType.electric},
+      {id: 3, name: 'Toyota Prius', color: 'Silver', type: CarType.electric},
+      {id: 4, name: 'Nissan Leaf', color: 'Green', type: CarType.electric},
+      {id: 5, name: 'Honda Civic', color: 'Red', type: CarType.gas},
+      {id: 6, name: 'Ford Mustang', color: 'Yellow', type: CarType.gas},
+      {id: 7, name: 'Chevrolet Bolt', color: 'Black', type: CarType.electric},
+      {id: 8, name: 'Audi e-tron', color: 'Gray', type: CarType.electric},
+      {id: 9, name: 'Mercedes-Benz C-Class', color: 'White', type: CarType.gas},
+      {id: 10, name: 'Kia Soul', color: 'Orange', type: CarType.gas},
+    ]));
+
+    this.parkingLot.set(new Map<number, ParkingLot>([
+      [1, {type: CarType.electric, car: null}],
+      [2, {type: CarType.electric, car: null}],
+      [3, {type: CarType.gas, car: null}],
+      [4, {type: CarType.gas, car: null}],
+      [5, {type: CarType.gas, car: null}],
+      [6, {type: CarType.gas, car: null}],
+    ]))
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {provideRouter, Routes} from '@angular/router';
 import {AppComponent} from "./app/app.component";
 
 export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/pages.component').then(c => c.PagesComponent) },
   { path: 'factory-async', loadComponent: () => import('./patterns/factory-async/parser-page/parser-page.component').then(c => c.ParserPageComponent) },
   { path: 'signals', loadComponent: () => import ('./features/signals/parking-lot/parking-lot.component').then(c => c.ParkingLotComponent) }
 ]
