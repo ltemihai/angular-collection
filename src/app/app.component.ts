4d3154a6c200244a682dbc4e84d95d158537d904
mihai.oltean <mihai.oltean@hibob.io>
2024-05-13 12:12:21 +0300

    Added side-nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatSidenavModule, MatButtonModule, MatListModule, MatIconModule],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
